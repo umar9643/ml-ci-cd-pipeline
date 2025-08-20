cd50ff4a18f962a8d7955d68aa8131a3a428e8a6
Umar Alam <alamumar258455@gmail.com>
2025-08-20 09:56:52 +0530

    initial commit version 1.1

---
+++ tests/test_model.py
@@ -0,0 +1,13 @@
+#test_model.py
+import unittest
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+
+class TestModelTraining(unittest.TestCase):
+    def test_model_training(self):
+        model = joblib.load('model/iris_model.pkl')
+        self.assertIsInstance(model, RandomForestClassifier)
+        self.assertGreaterEqual(len(model.feature_importances_), 4)
+
+if _name_ == '_main_':
+    unittest.main()
